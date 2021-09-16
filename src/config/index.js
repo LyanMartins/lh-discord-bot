9fe8d25690d4ce486a0cbbf05c10ac15cf847e49
Lyan Martins <lyan.marcelino@clinicarx.com.br>
2021-09-15 23:12:47 -0300

    fix code

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    botToken: 'ODg3ODc4Nzg5OTMyMTQyNjUy.YUKjjw.R2bS1jDlk42nsBZ_OeePm3T2764',
+    botToken: 'ODg3ODc4Nzg5OTMyMTQyNjUy.YUKjjw.RVCC4BS0tJHhFu23wefhk62cm_U',
     prefix: '!',
     defaultLocale: 'pt-BR',
     disabledEvents: [
