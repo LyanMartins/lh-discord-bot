a41cde78548ccccf3ec6dcdaa9884e1023120c2d
Lyan Martins <lyan.martins3@gmail.com>
2021-09-15 22:56:49 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    botToken: '',
+    botToken: 'ODg3ODc4Nzg5OTMyMTQyNjUy.YUKjjw.R2bS1jDlk42nsBZ_OeePm3T2764',
     prefix: '!',
     defaultLocale: 'pt-BR',
     disabledEvents: [
